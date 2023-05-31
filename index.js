17be47024d3c0ff3aa76c00769f967e521672a27
joha4022 <joha4022@gmail.com>
2023-05-31 10:55:12 -0700

    added vending machine test

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ class Queue {
 
     hasEvenNumbers() {
         var evenElement = this._array.find(element => {
-            return element%2 === 0;
+            return element % 2 === 0;
         });
-        if(evenElement)
+        if (evenElement)
             return true;
         return false;
     }
@@ -26,4 +26,68 @@ class Queue {
     }
 }
 
-module.exports = Queue;
+class Item {
+    constructor(name, price, code) {
+        this.name = name;
+        this.price = price;
+        this.code = code;
+    }
+};
+
+class VendingMachine {
+    constructor() {
+        this.snacks = []
+        this.customerFunds = 0
+    }
+
+    seeSelections() {
+        // displays all available snacks
+        return this.snacks;
+    }
+
+    stock(newSnacks = []) {
+        // adds input array of snacks to exisitng snacks
+        this.snacks = this.snacks.concat(newSnacks);
+    }
+
+    deposit(amount) {
+        // updates existing customerFunds with the amount deposited
+        this.customerFunds += amount;
+    }
+
+    giveChange() {
+        // updates customerFunds to 0 and returns the remaining amount
+        const currentFunds = this.customerFunds;
+        this.customerFunds = 0;
+        return currentFunds;
+    }
+
+    buy(snackName) {
+        // updates inventory with bought snacks
+        // updates customerFunds to reflect purchase
+        // notifies customer if a snack is unavailable
+        // notifies customer when a snack costs more than available customerFunds
+        // clears out customerFunds by calling giveChange()
+        let foundSnack = false;
+        for (let i = 0; i < this.snacks.length; i++) {
+            let item = this.snacks[i];
+            if (item.name === snackName) {
+                if (this.customerFunds - item.price < 0) {
+                    return 'Not enough funds available';
+                }
+                foundSnack = true;
+                this.customerFunds -= this.snacks[i].price;
+                this.snacks.splice(i, 1);
+                break;
+            }
+        }
+        if (!foundSnack) {
+            return `${snackName} is not available.`;
+        }
+        return this.giveChange();
+    }
+}
+
+module.exports.Queue = Queue;
+module.exports.Item = Item;
+module.exports.VendingMachine = VendingMachine;
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-let Queue = require('./index.js');
+let { Queue, Item, VendingMachine } = require('./index');
 
 // describe helps us to organize the test by grouping similar tests together
 describe('queue test', () => {
@@ -25,7 +25,69 @@ describe('queue test', () => {
     test('should remove all even numbers inside of an array', () => {
         let oddNumbers = queue.removeEvenNumbers();
 
-        expect(oddNumbers).toEqual([9,7,13]);
+        expect(oddNumbers).toEqual([9, 7, 13]);
     })
 })
 
+
+let snack1, snack2, vendingMachine;
+
+describe('Vending Machine', () => {
+  beforeEach(() => {
+  //beforeEach hook runs before every test, creating new instances of Item and VendingMachine for you to use in your tests
+    snack1 = new Item('Potato Chips', 1.5, 1);
+    snack2 = new Item('Chocolate Bar', 2.5, 2);
+    vendingMachine = new VendingMachine();
+  })
+  // YOUR TEST CASES GO HERE
+  test('.seeSelections(): display all available snacks', () => {
+    expect(vendingMachine.seeSelections()).toEqual([]);
+    vendingMachine.stock(newSnacks = [snack1, snack2]);
+    expect(vendingMachine.seeSelections()).toEqual([snack1, snack2]);
+
+  })
+
+  test('.stock: adds input array of snacks to existing snacks', () => {
+    vendingMachine.stock(newSnacks = [snack1, snack2])
+    expect(vendingMachine.snacks).toEqual([snack1, snack2]);
+  })
+  
+  test('.deposit(): updates existing customerFunds with the amount deposited', () => {
+    vendingMachine.deposit(20);
+    expect(vendingMachine.customerFunds).toBe(20);
+  })
+
+  test('.giveChange(): updates customerFunds to 0 and returns the remaining amount', () => {
+    vendingMachine.deposit(20);
+    expect(vendingMachine.giveChange()).toBe(20);
+    expect(vendingMachine.customerFunds).toBe(0);
+  })
+
+  test(`.buy(): updates inventory with bought snacks`, () => {
+    vendingMachine.stock(newSnacks = [snack1, snack2]);
+    vendingMachine.deposit(20);
+    vendingMachine.buy('Potato Chips');
+    expect(vendingMachine.snacks).toEqual([snack2]);
+    expect(vendingMachine.customerFunds).toBe(0);
+  })
+
+  test(`.buy(): updates customerFunds to reflect purchase
+            : clears out customerFund by calling giveChange()`, () => {
+    vendingMachine.stock(newSnacks = [snack1, snack2]);
+    vendingMachine.deposit(20);
+    expect(vendingMachine.buy('Potato Chips')).toBe(18.5);
+    expect(vendingMachine.customerFunds).toBe(0);
+  })
+
+  test(`.buy(): notifies customer if a snack is unavailable`, () => {
+    vendingMachine.stock(newSnacks = [snack2]);
+    vendingMachine.deposit(20);
+    expect(vendingMachine.buy('Potato Chips')).toBe('Potato Chips is not available.');
+  })
+
+  test(`.buy(): notifies customer when a snack cost more than available customerFunds`, () => {
+    vendingMachine.stock(newSnacks = [snack1, snack2]);
+    vendingMachine.deposit(0.5);
+    expect(vendingMachine.buy('Potato Chips')).toBe('Not enough funds available');
+  })
+});
